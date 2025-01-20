be57b8a72d1758582c5f57758b10310099697457
Leah Sommer <sommer.leah@googlemail.com>
2025-01-20 16:23:51 +0100

    reworked tutorial on marscene + sounds for mars and space

--- VR Game/Assets/Leonid/SoundSpace.cs
+++ VR Game/Assets/Leonid/SoundSpace.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
 
 public class SoundSpace : MonoBehaviour
-{
-    [SerializeField] private AudioClip sceneMusicClip;
-    [SerializeField] private float musicVolume = 0.2f;
+{ [SerializeField] private AudioClip sceneMusicClip; // Background music clip
+    [SerializeField] private float musicVolume = 0.2f; // Background music volume
+
+    [SerializeField] private AudioClip startSoundClip; // Sound to play at the start
+    [SerializeField] private float startSoundVolume = 1.0f; // Volume of the start sound
 
     void Start()
     {
-        // If SoundManager exists, tell it to play the scene's music
+        // Play the start sound
+        if (startSoundClip != null)
+        {
+            AudioSource.PlayClipAtPoint(startSoundClip, transform.position, startSoundVolume);
+        }
+
+        // Play the background music if SoundManager exists
         if (SoundManager.Instance != null)
         {
             SoundManager.Instance.PlayBackgroundMusic(sceneMusicClip, musicVolume);
